c46d0456a9be5f6aad993336ee6092eb4dc600e5
kwizl <karis.njoroge@gmail.com>
2020-06-12 22:22:24 +0300

    Add rspec tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'User Associations' do
+    it { should have_many(:articles) }
+  end
+  context 'User Validation tests' do
+    let(:user) { build(:user) }
+
+    it 'should save user' do
+      expect(user.save).to be(true)
+    end
+  end
 end
