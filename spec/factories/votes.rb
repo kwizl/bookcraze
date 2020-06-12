804ee7ab0e49ea5fff5f71e33d7abbbf40741597
kwizl <karis.njoroge@gmail.com>
2020-06-12 23:33:25 +0300

    Add rspec tests for model Vote

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :vote do
-    user { nil }
-    article { nil }
+    user
+    article
   end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  
+  context 'Vote Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:article) }
+  end
 end
