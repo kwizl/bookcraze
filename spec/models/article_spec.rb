2ea7551698bf70d0589965a7c50142f2be399295
kwizl <karis.njoroge@gmail.com>
2020-06-12 23:40:44 +0300

    Added rspec validations tests to model Article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -14,5 +14,17 @@ RSpec.describe Article, type: :model do
       article_data = user.articles.build(article)
       expect(article_data.save).to be(true)
     end
+
+    it 'should not save article with empty title' do
+      article_data = user.articles.build(article)
+      article_data.title = nil
+      expect(article_data.save).to be(false)
+    end
+
+    it 'should not save article with empty text' do
+      article_data = user.articles.build(article)
+      article_data.text = nil
+      expect(article_data.save).to be(false)
+    end
   end
 end
