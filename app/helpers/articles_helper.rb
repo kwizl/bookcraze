88c7753067fe59ee002df39d7f9f9198697c0e26
kwizl <karis.njoroge@gmail.com>
2020-06-18 01:34:40 +0300

    Remove logic from article index page

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,33 @@
 module ArticlesHelper
   include ApplicationHelper
 
-  def category_articles_helper
-
+  def events_list(events)
+    html = ''
+    events.each do |e|
+      html += <<-HTML
+      <tr scope="row">
+        <td>#{e.name}</td>
+        <td>#{e.description}</td>
+        <td>#{e.date}</td>
+        <td>#{e.creator.name}</td>
+        <td>#{link_to 'view', e}</td>
+      </tr>
+      HTML
+    end
+    html.html_safe
+  end
+  def category_articles_helper(categories)
+    html = ''
+    categories.each do |c|
+      html += <<-HTML
+      <div class='col-md-4'>
+      #{link_to c.name, category_path(c.id)}
+      #{image_tag c.thumbnail if c.image.attached?}
+      #{c.text}
+      </div>
+      HTML
+    end
+    html.html_safe
+    
   end
 end
