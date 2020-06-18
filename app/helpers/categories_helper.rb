873bcc325b4070872dea685bf55040c965194bc6
kwizl <karis.njoroge@gmail.com>
2020-06-18 19:50:43 +0300

    Style the articles show page

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,7 @@
 module CategoriesHelper
+  def truncate(text, length = 30, truncate_string = "...")
+    if text.nil? then return end
+    l = length - truncate_string.chars.to_a.size
+    (text.chars.to_a.size > length ? text.chars.to_a[0...l].join + truncate_string : text).to_s
+  end
 end
