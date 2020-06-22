8d5c69928f0303304e0387d18bf5876d7e40683e
kwizl <karis.njoroge@gmail.com>
2020-06-23 00:10:12 +0300

    Validate that priority is bewteen 1 and 4

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   validates :priority, presence: true
+  validates :priority, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 4 }
 
   def self.category_articles(category_id)
     find_by_sql(['SELECT * FROM categories WHERE id = ?', category_id])
