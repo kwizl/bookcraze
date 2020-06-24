82829b771292df8a9f7757e23f972105a8ac532b
kwizl <karis.njoroge@gmail.com>
2020-06-25 00:51:30 +0300

    Add instructions to README.md

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_06_19_134204) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -37,13 +34,13 @@ ActiveRecord::Schema.define(version: 2020_06_19_134204) do
   end
 
   create_table "articles", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.text "title"
     t.text "text"
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "category_id"
+    t.integer "category_id"
     t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
@@ -51,7 +48,7 @@ ActiveRecord::Schema.define(version: 2020_06_19_134204) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "priority"
-    t.bigint "article_id", null: false
+    t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_categories_on_article_id"
@@ -64,8 +61,8 @@ ActiveRecord::Schema.define(version: 2020_06_19_134204) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "article_id", null: false
+    t.integer "user_id", null: false
+    t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_votes_on_article_id"
