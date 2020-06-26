01584f624ac89d995bb41c46a8b13176401c2131
kwizl <karis.njoroge@gmail.com>
2020-06-27 02:20:48 +0300

    Add custom form select on articles nested form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,24 @@ class ApplicationController < ActionController::Base
   def find_user
     @current_user = User.find_by(name: params[:name])
   end
+
+  def category_one
+    Category.find_by_sql(['SELECT * FROM articles a JOIN categories c ON a.id = c.article_id
+      WHERE c.priority = 1 ORDER BY a.created_at DESC LIMIT 1'])
+  end
+
+  def category_two
+    Category.find_by_sql(['SELECT * FROM articles a JOIN categories c ON a.id = c.article_id
+      WHERE c.priority = 2 ORDER BY a.created_at DESC LIMIT 1'])
+  end
+
+  def category_three
+    Category.find_by_sql(['SELECT * FROM articles a JOIN categories c ON a.id = c.article_id
+      WHERE c.priority = 3 ORDER BY a.created_at DESC LIMIT 1'])
+  end
+
+  def category_four
+    Category.find_by_sql(['SELECT * FROM articles a JOIN categories c ON a.id = c.article_id
+      WHERE c.priority = 4 ORDER BY a.created_at DESC LIMIT 1'])
+  end
 end
