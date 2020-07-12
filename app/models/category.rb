a14b768ac7336c7f024f9048adc5c60920381986
kwizl <karis.njoroge@gmail.com>
2020-07-12 16:27:58 +0300

    Add trigger to the table category and column priority

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,6 @@ class Category < ApplicationRecord
   belongs_to :article
 
   validates :name, presence: true
-  validates :priority, presence: true
-  validates :priority, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
 
   def self.category_articles(category_id)
     find_by_sql(['SELECT * FROM categories WHERE id = ?', category_id])
